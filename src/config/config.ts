b16342bf45008367d5ec0d29ac0e95bf3aa50532
sglkbrk <sglk.brk@gmail.com>
2025-12-09 23:53:31 +0300

    docker network

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,8 +2,8 @@ const config = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
 
   apiEndpoints: {
-    downloadFile: process.env.NEXT_PUBLIC_SITE_URL + '/api/MinioFile/download/',
-    imageUrl: process.env.NEXT_PUBLIC_API_URL + '/api/MinioFile/download/'
+    downloadFile: process.env.NEXT_PUBLIC_IMAGE_URL + '/api/MinioFile/download/',
+    imageUrl: process.env.NEXT_PUBLIC_IMAGE_URL + '/api/MinioFile/download/'
   }
 };
 
